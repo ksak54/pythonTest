ce543869d85b92d6736cef7a0d999d2f15dcdeb5
Marshadow <30525565+ksak54@users.noreply.github.com>
2020-04-10 14:14:08 +0530

    Initial commit

---
+++ hPPrediction.py
@@ -0,0 +1,11 @@
+import sys
+print(sys.version)
+print(sys.executable)
+
+
+def greeting(name):
+    print("Welcome {} to the machine".format(name))
+    print('Hello World')
+
+
+greeting("Saksham")
